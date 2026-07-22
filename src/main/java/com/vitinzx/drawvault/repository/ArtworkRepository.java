545851669352a1878f67f8404aaae4bcc356d1fd
vitinzx7 <victordideus@gmail.com>
2026-07-22 05:24:58 -0300

    feat: add public artwork query

--- src/main/java/com/vitinzx/drawvault/repository/ArtworkRepository.java
+++ src/main/java/com/vitinzx/drawvault/repository/ArtworkRepository.java
@@ -1,5 +1,6 @@
 package com.vitinzx.drawvault.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vitinzx.drawvault.entity.Artwork;
 
 public interface ArtworkRepository extends JpaRepository<Artwork, UUID> {
+    List<Artwork> findByVisibleTrueOrderByCreatedAtDesc();
 }
