3ae6abe5beab2f26e1e3ee34f5902b0ba14a57aa
vitinzx7 <victordideus@gmail.com>
2026-07-29 07:51:41 -0300

    feat(db): seed public artworks

---
+++ src/main/resources/db/migration/V2__seed_public_artworks.sql
@@ -0,0 +1,101 @@
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742001',
+        'Meliodas X Zeldris',
+        '',
+        '/artworks/meliodas-x-zeldris.webp',
+        true,
+        CURRENT_TIMESTAMP
+       );
+
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742002',
+        'Anime garota',
+        '',
+        '/artworks/anime-garota.webp',
+        TRUE,
+        CURRENT_TIMESTAMP - INTERVAL '1 second'
+       );
+
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742003',
+        'Ravena',
+        '',
+        '/artworks/ravena.webp',
+        TRUE,
+        CURRENT_TIMESTAMP - INTERVAL '2 seconds'
+       );
+
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742004',
+        'Ghost',
+        '',
+        '/artworks/ghost.webp',
+        TRUE,
+        CURRENT_TIMESTAMP - INTERVAL '3 seconds'
+       );
+
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742005',
+        'Midoriya',
+        '',
+        '/artworks/midoriya.webp',
+        TRUE,
+        CURRENT_TIMESTAMP - INTERVAL '4 seconds'
+       );
+
+INSERT INTO artworks (
+    id,
+    name,
+    description,
+    image_url,
+    visible,
+    created_at
+)
+VALUES (
+        'f4a4d7d4-bfe7-4af5-9e34-c44c4b742006',
+        'Obito',
+        '',
+        '/artworks/obito.webp',
+        TRUE,
+        CURRENT_TIMESTAMP - INTERVAL '5 seconds'
+       );
